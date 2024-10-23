b71d9d5701350fa6367222d25a26da7aca232062
PeriklisLazarides <126088575+PapasGaming1234@users.noreply.github.com>
2024-10-23 13:07:35 +0100

    added healthbar asset and c++ class

--- Source/GAMR3510Assignment/Private/HealthBarWidget.cpp
+++ Source/GAMR3510Assignment/Private/HealthBarWidget.cpp
@@ -2,4 +2,21 @@
 
 
 #include "HealthBarWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
 
+
+void UHealthBarWidget::UpdateHealth(float CurrentHealth, float MaxHealth)
+{
+    UE_LOG(LogTemp, Warning, TEXT("function has been called"));
+    CurrentHealth = FMath::Clamp(CurrentHealth, 0.0f, 1.0f);
+
+    if (HealthBar) {
+        HealthBar->SetPercent(CurrentHealth);
+    }
+
+    if (HealthText) {
+        HealthText->SetText(FText::AsPercent(HealthBar->GetPercent()));
+        // HealthAmount->SetText(FText::AsNumber(CurrentHealth));
+    }
+}
--- Source/GAMR3510Assignment/Public/HealthBarWidget.h
+++ Source/GAMR3510Assignment/Public/HealthBarWidget.h
@@ -14,4 +14,15 @@ class GAMR3510ASSIGNMENT_API UHealthBarWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+    // The progress bar that shows the health
+    UPROPERTY(BlueprintReadWrite, meta = (BindWidget)) // Binding this with the widget that has the same name
+        class UProgressBar* HealthBar;  // Progress bar
+
+    // The text block that shows the percentage
+    UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+        class UTextBlock* HealthText;  // Text that shows health
+
+    // Function to update both the progress bar and text
+    void UpdateHealth(float CurrentHealth, float MaxHealth);
 };
